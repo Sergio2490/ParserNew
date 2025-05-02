8499b2b8f6fcde19ea68f52e6ecd6ca70ee4f33b
Sergio2490 <merido2490@gmail.com>
2025-05-03 02:08:28 +0300

    PS02_lesson

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import pprint
+response = requests.get("https://api.github.com")
+# print(response.status_code)
+# if response.ok:
+#     print("запрос выполнен успешно")
+# else:
+#     print("произошла ошибка")
+# print(response.text)
+print(response.text)
+response_json = response.json()
+pprint.pprint(response_json)
+
+#dict = {
+#    "url": "https://www.google.com",
+#    "title": "Google"
+#}
