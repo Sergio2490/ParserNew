e5baa0359b69ae861a7efded01bd82b1c9c7e71b
Sergio2490 <merido2490@gmail.com>
2025-05-20 01:00:41 +0300

    Full_OP03_2

--- OP03_2.py
+++ OP03_2.py
@@ -7,4 +7,12 @@ response = requests.get(url)  #отправляем Get-запрос
 html = response.text  #получаем html код странички, сохр его в перем.html и далее - с этим кодом работаем
 soup = BeautifulSoup(html, "html.parser")
 
+text = soup.find_all("span", class_ ="text")
+#print(text)
+author = soup.find_all("small", class_ ="author")
+#print(author)
 
+for i in range(len(text)):
+    print(f"Цитата номер - {i+1}")
+    print(text[i].text)
+    print(f"Автор цитаты - {author[i].text}\n")
