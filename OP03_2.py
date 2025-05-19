2d8937bc1b38053d99681fe434f5e6407e948eda
Sergio2490 <merido2490@gmail.com>
2025-05-19 22:15:49 +0300

    Upd_op03_2

--- OP03_2.py
+++ OP03_2.py
@@ -1,26 +1,10 @@
-import re
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-# Получаем HTML-код страницы
-url = "https://минюст43.рф"  # Замените на нужный URL
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+url = "http://quotes.toscrape.com/"
 
-# Регулярное выражение для поиска адресов электронной почты
-email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+response = requests.get(url)  #отправляем Get-запрос
+html = response.text  #получаем html код странички, сохр его в перем.html и далее - с этим кодом работаем
+soup = BeautifulSoup(html, "html.parser")
 
-# Используем find_all() для поиска всех текстов, соответствующих шаблону
-text_elements = soup.find_all(string=re.compile(email_pattern))
 
-# Сохраняем найденные email в набор для удаления дубликатов
-emails = set()
-
-# Извлекаем адреса электронной почты из найденных текстовых элементов
-for element in text_elements:
-    found_emails = re.findall(email_pattern, element)
-    emails.update(found_emails)
-
-# Выводим найденные email
-for email in emails:
-    print(email)
