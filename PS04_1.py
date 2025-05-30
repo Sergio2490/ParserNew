01e495c2d48f4ba29c0fc1fd43a56482cb15d463
Sergio2490 <merido2490@gmail.com>
2025-05-31 00:15:51 +0300

    PS04_1_Screenshots_Refresh

--- PS04_1.py
+++ PS04_1.py
@@ -3,5 +3,12 @@ import time
 
 browser = webdriver.Firefox()
 browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+browser.save_screenshot("dom.png")
+
 time.sleep(10)
-browser.quit()
+browser.get("https://ru.wikipedia.org/wiki/Selenium")
+browser.save_screenshot("selenium.png")
+time.sleep(3)
+browser.refresh()
+
+
