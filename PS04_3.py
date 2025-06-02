9631286b50af2a5cb45d5227740c27579f23318e
Sergio2490 <merido2490@gmail.com>
2025-06-03 00:11:59 +0300

    Create PS04_3.py

---
+++ PS04_3.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver import Keys  #для ввода текста на сайте в клавиатуры
+from selenium.webdriver.common.by import By  # для поиска эл-тов на странице с пом DOM
+import time
+
+browser = webdriver.Firefox()  # далее вставим ссылку на страницу сразу с Солнечной системой
+browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
+
