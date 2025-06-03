247ab14eb9b8c2ffea2f9b430556fa5dd8331716
Sergio2490 <merido2490@gmail.com>
2025-06-03 23:59:02 +0300

    Paragraphs_Site

--- PS04_3.py
+++ PS04_3.py
@@ -6,3 +6,8 @@ import time
 browser = webdriver.Firefox()  # далее вставим ссылку на страницу сразу с Солнечной системой
 browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
 
+paragraphs = browser.find_elements(By.TAG_NAME, "p") #ищем ВСЕ элементы с тэгом <p> на страничке
+for paragraph in paragraphs:
+    print(paragraph.text) # выводим текст очередного параграфа
+    input() # пучтой инпут - по нажатии Enter выводим  текст из следующего параграфа
+
