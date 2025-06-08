0cdb766f50ba50a897246c4bf0c3c2cfca0db8c3
Sergio2490 <merido2490@gmail.com>
2025-06-08 22:22:31 +0300

    Update_Dz_PS04_2025_06_08

--- Dz_PS04.py
+++ Dz_PS04.py
@@ -23,6 +23,7 @@ answer = answer.capitalize()
 assert "Википедия" in browser.title  # Проверка на наличие заголовка "Википедия" в теге <title>Википедия
 time.sleep(3)
 
+
 # Сейчас б. искать строку ввода в Википедии  <input id = "searchInput">
 search_box = browser.find_element(By.ID, "searchInput")
 search_box.send_keys(answer)  #в строку поиска вводим то, что ввел пользователь
@@ -32,6 +33,48 @@ time.sleep(3)
 # сейчас п.3 - щелкаем на первую статью b dsdjlbv vty. bp 3-[ geyrnjd/
 a = browser.find_element(By.LINK_TEXT, answer) #в переменную a - первый найденный элемент
 a.click()
+
+def second_point():
+    #Если выбран Пункт 2. Перед этим юзер ввел слово, мы вывели список статей и зашли в первую (щелкнули по первой ссылке)
+    #Далее мы поместим в список все Основные статьи на этой странице и перейдем в рандомную
+
+    hatnotes = []   #список для хранения Основных статей странички
+    for element in browser.find_elements(By.TAG_NAME, "div"): #мы б.перебирать список элементов с тэгами div
+        cl = element.get_attribute("class")  #берем очередной элемент тэга div  и ищем у него атрибут class
+        if cl == "hatnote navigation-not-searchable ts-main": #если cl (класс) hfdty "название класса", то мы вставляем этот элемент в список hatnotes[]
+            hatnotes.append(element)
+    #в итоге соберем список всех эл-тов с таким названием класса (Основных статей)
+    #print(hatnotes)
+    hatnote = random.choice(hatnotes) #в переменной- случайный элемент из списка
+    link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")    #сейчас нам нужно перейти по ссылке. У нас есть тэг div, а внутри него - тэг a со ссылкой. Т.е. внутри тэга div ищем тэг a и переходим по нему
+    browser.get(link)  # Мы открыли случайную Основную статью
+
+    # выводим меню из 3-х пунктов
+    user_choice1 = input("Выберите дальнейшие действия: \n1 - листать параграфы статьи (по Enter)\n2 - перейти на одну из внутренних статей")
+    if user_choice1 == "1":
+        paragraphs = browser.find_elements(By.TAG_NAME, "p")
+        for paragraph in paragraphs:
+            print(paragraph.text)
+            br = input()
+            if br == "3":
+                exit()
+    elif user_choice1 == "2":
+        #Здесь нам нужно внутри уже открытой ранее(перед if) статьи перейти по пункту меню на случайную внутреннюю статью
+        contents = []  #список элементов внутренних статей
+        for element in browser.find_elements(By.TAG_NAME, "div"):
+                el = element.get_attribute("class")
+                if el == "toctitle":
+                    contents.append(element)
+        print(contents)
+
+        #Сейчас выбираем случайный элемент списка(пункт содержание)
+        content = random.choice(contents)
+        link = content.find_element(By.TAG_NAME, "a").get_attribute("href")
+        browser.get(link)
+        print(link.text)
+
+    return
+
 
 #выводим меню из 3-х пунктов
 user_choice = input("Выберите дальнейшие действия: \n1 - листать параграфы текущей статьи (по Enter)\n2 - перейти на одну из связанных страниц (рандомно)\n3 - выйти из программы")
@@ -43,6 +86,7 @@ if user_choice =="1":
         if br == "3":
             exit()
 elif user_choice == "2":
-    pass
+    second_point()
 elif user_choice == "3":
     exit()
+
