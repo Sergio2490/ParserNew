2e2a1df68a9752f8fc23eff7eb605d29ce1598ea
Sergio2490 <merido2490@gmail.com>
2025-06-25 00:43:37 +0300

    Finished_PS05Vid_3

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -11,6 +11,6 @@ class DivannewparsSpider(scrapy.Spider):
         for divan in divans:
               yield {
                   "name": divan.css("div.lsooF span::text").get(),
-                  "price": divan.css("div.pY3d2 span::text").get,
-
+                  "price": divan.css("div.pY3d2 span::text").get(),
+                  "url" : divan.css("a").attrib['href']
               }
